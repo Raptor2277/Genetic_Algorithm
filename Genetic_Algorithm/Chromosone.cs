d37172172e69bbabc8193e2bc1b818ebf7eadd17
Raptor2277 <stadnitchii@yahoo.com>
2016-03-13 17:45:29 -0400

    Revert "Revert "First Sync""
    This reverts commit c9cd104f9eb4f576592a6d004a90debc0748af72.

---
+++ Genetic_Algorithm/Chromosone.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genetic_Algorithm
+{
+    abstract class Chromosone
+    {
+        public Gene[] Genes { get; protected set; }
+
+        public float Fitness { get; protected set; }
+
+        public abstract Chromosone CrossOver(Chromosone b);
+
+        public abstract void CalculateFitness();
+
+        public abstract void Mutate(Object obj);
+
+        #region Operators
+        public static bool operator >(Chromosone left, Chromosone right)
+        {
+            return left.Fitness > right.Fitness;
+        }
+
+        public static bool operator >=(Chromosone left, Chromosone right)
+        {
+            return left.Fitness >= right.Fitness;
+        }
+
+        public static bool operator <(Chromosone left, Chromosone right)
+        {
+            return left.Fitness < right.Fitness;
+        }
+
+        public static bool operator <=(Chromosone left, Chromosone right)
+        {
+            return left.Fitness <= right.Fitness;
+        }
+
+        public static bool operator ==(Chromosone left, Chromosone right)
+        {
+            return left.Fitness == right.Fitness;
+        }
+
+        public static bool operator !=(Chromosone left, Chromosone right)
+        {
+            return left.Fitness != right.Fitness;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        #endregion
+    }
+
+    public struct Gene
+    {
+        public double Value;
+        public string Name;
+        public Gene(string name, double value)
+        {
+            Value = value;
+            Name = name;
+        }
+    }
+}
